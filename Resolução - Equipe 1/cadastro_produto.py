c4af204b393616c632cf3b333741c376c22e5aac
usrmaia <georgemaiaf@gmail.com>
2022-05-09 14:50:01 -0300

    revisão cadastro_produto

--- Resolução - Equipe 1/cadastro_produto.py
+++ Resolução - Equipe 1/cadastro_produto.py
@@ -1,6 +1,7 @@
 import bd_vai_vuado, util
 
 class Cadastro():
+    
     def __init__(self, id_adm_empresa, bd):
         self.nome = []
         self.categoria = []
@@ -9,21 +10,28 @@ class Cadastro():
         self.descricao = []
         self.bd = bd
         self.id_adm_empresa = id_adm_empresa
+        self.opcao = []
     
     def inserirInformacoesDoProduto(self):
 
         while(True):
 
-            util.exibirMenuDeProdutoSimples(self.nome, self.categoria, self.medidas, self.imagem, self.descricao)
-            opcao = input("Selecione uma opção: ")
+            util.exibirInformacoesDeProduto([self.nome, self.categoria, self.medidas, self.imagem, self.descricao], "0. Sair 6. Confirmar")
+            self.opcao = input("Selecione uma opção: ")
 
-            match opcao:
+            match self.opcao:
                 case "1": self.nome = input("Informe o nome do produto: ")
                 case "2": self.categoria = input("Informe a categoria do produto: ") 
                 case "3": self.medidas = input("Informe as medidas do produto (peso - largura x altura x profundidade): ")
                 case "4": self.imagem = input("Informe o link para imagem do produto: ")
                 case "5": self.descricao = input("Informe uma descrição para o produto: ")
-                case _: break
+                case "6": 
+                    self.opcao = "Confirmar"
+                    break
+                case "0":
+                    self.opcao = "Sair"
+                    break
+                case _: pass
     
     def validarCadastroDeProduto(self):
 
@@ -32,6 +40,7 @@ class Cadastro():
         for dado in dados:
 
             if len(dado) == 0: 
+
                 print("Há dado(s) não cadastrado(s)!")
 
                 return False
@@ -39,25 +48,33 @@ class Cadastro():
         return True
     
     def confirmarCadastro(self):
-        opcao = input("""
 
-                        1. Confirmar
-                        2. Limpar Dados
-                        3. Cancelar
-                        0. Voltar
+        while(True):
 
-                        Selecione uma opção: 
+            self.opcao = input("""
 
-                """)
+                            1. Confirmar
+                            2. Limpar Dados / Cancelar
+                            0. Sair
 
-        match opcao:
-            case "1": 
-                self.bd.inserirProduto(self.id_adm_empresa, self.nome, self.categoria, self.medidas, self.imagem, self.descricao)
-            case "2":
-                self.nome = self.categoria = self.medidas = self.imagem = self.descricao = []
-                return "Limpar Dados"
-            case _: pass
-            
+                            Selecione uma opção: 
+
+                    """)
+
+            match self.opcao:
+
+                case "1": 
+                    self.bd.inserirProduto(self.id_adm_empresa, self.nome, self.categoria, self.medidas, self.imagem, self.descricao)
+                    break
+                case "2": break
+                case "0": 
+                    self.opcao = "Sair"
+                    break 
+                case _: pass
+
+    def redefinirDados(self):
+        self.nome = self.categoria = self.medidas = self.imagem = self.descricao = []
+                
 if __name__ == "__main__":
     bd = bd_vai_vuado.BD()
     id_adm_empresa = 1
--- Resolução - Equipe 1/implementacao_cadastro.py
+++ Resolução - Equipe 1/implementacao_cadastro.py
@@ -1,18 +1,30 @@
-import cadastrar_produto, bd_vai_vuado
+import cadastro_produto, bd_vai_vuado
 
 class Cadastro():
+
     def __init__(self, id_adm_empresa, bd):
         self.id_adm_empresa = id_adm_empresa
         self.bd = bd
-        self.cadastro = cadastrar_produto.Cadastro(self.id_adm_empresa, self.bd)
+        self.cadastro = cadastro_produto.Cadastro(self.id_adm_empresa, self.bd)
     
     def cadastrarProduto(self):
         while(True):
+
             self.cadastro.inserirInformacoesDoProduto()
-            if self.cadastro.validarCadastroDeProduto():
-                resposta = self.cadastro.confirmarCadastro()
-                if resposta != "Limpar Dados": break
-            else: break
+
+            if self.cadastro.opcao == "Confirmar":
+
+                if self.cadastro.validarCadastroDeProduto():
+
+                    self.cadastro.confirmarCadastro()
+
+                    if self.cadastro.opcao == "Sair": break 
+
+                    self.cadastro.redefinirDados()
+
+                else: pass
+            
+            elif self.cadastro.opcao == "Sair": break
 
 if __name__ == "__main__":
     bd = bd_vai_vuado.BD()
